12554ebcb8828f677da67ddd85cda8e6fce9af0f
Alisson <alissonjscosta@gmail.com>
2024-02-07 13:58:03 -0300

    initialProject

---
+++ microfront-about/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>About</h1>
+        <p>Esta é a página "Sobre"!</p>
+      </header>
+    </div>
+  );
+}
+
+export default About;
---
+++ microfront-home/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Home</h1>
+        <p>Bem-vindo à página inicial!</p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
